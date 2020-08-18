ca1fd9ad2830abf3935f0c403e612fefce64b997
Mark Needham <m.h.needham@gmail.com>
2020-08-18 21:52:02 +0100

    add script to dl notebooks

---
+++ scripts/download-lp-notebooks.js
@@ -0,0 +1,21 @@
+const http = require('https');
+const fs = require('fs');
+
+const download = function(url, dest, cb) {
+  var file = fs.createWriteStream(dest);
+  var request = http.get(url, function(response) {
+    response.pipe(file);
+    file.on('finish', function() {
+      file.close(cb);  // close() is async, call cb after close completes.
+    });
+  }).on('error', function(err) { // Handle errors
+    fs.unlink(dest); // Delete the file async. (But we don't check the result)
+    if (cb) cb(err.message);
+  });
+};
+
+const remotePrefix = "https://raw.githubusercontent.com/neo4j-examples/link-prediction/master/notebooks/"
+const localPrefix = "modules/graph-data-science/examples/link-prediction/"
+
+const files = ["03_Train_Test_Split.py", "04_Model_Feature_Engineering.py", "05_Train_Evaluate_Model.py"]
+files.forEach(value => download(remotePrefix + value, localPrefix + value))
