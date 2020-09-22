54920c38920720fc9f7ffadd64fa6ab5630271df
Mark Needham <m.h.needham@gmail.com>
2020-09-22 16:25:45 +0100

    Sagemaker (#29)
    * sagemaker
    
    * update guide
    
    * update file name
    
    * tidy up
    
    * update with eval
    
    * update code example
    
    * add text
    
    * update files to download

--- scripts/download-lp-notebooks.js
+++ scripts/download-lp-notebooks.js
@@ -21,9 +21,14 @@ const files = ["py/02_Co-Author_Graph.py",
                "py/03_Train_Test_Split.py",
                "py/04_Model_Feature_Engineering.py",
                "py/05_Train_Evaluate_Model.py",
+               "py/06_SageMaker.py",
                "notebooks/data/model-eval.csv",
                "notebooks/data/df_test_under_sample.csv",
                "notebooks/data/df_train_under_sample.csv",
                "notebooks/data/df_test_under_basic_sample.csv",
-               "notebooks/data/df_train_under_basic_sample.csv"]
+               "notebooks/data/df_train_under_basic_sample.csv",
+               "notebooks/data/download/autopilot_best_candidate.csv",
+               "notebooks/data/download/autopilot_candidates.csv",
+               "notebooks/data/download/inference_results.csv",
+               "notebooks/data/autopilot_candidates.csv"]
 files.forEach(value => download(remotePrefix + value, localPrefix + value))
