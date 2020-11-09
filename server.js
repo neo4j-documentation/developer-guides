1c141c502cbe327c056a9e99b3cca849d723854c
Adam Cowley <github@adamcowley.co.uk>
2020-11-09 12:24:59 +0000

    More updates to the landing page

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.use(express.static('./build/site'))
 
 app.use('/static/assets', express.static('./build/site/_'))
 
-app.get('/', (req, res) => res.redirect('/developer/get-started'))
+app.get('/', (req, res) => res.redirect('/developer/'))
 
 app.listen(8000, () => console.log('📘 http://localhost:8000'))
